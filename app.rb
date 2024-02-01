285909dcbbfc0bc213b05a90028151a97b2d5eeb
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-02-01 17:15:43 +0000

    trying to connect to api

--- app.rb
+++ app.rb
@@ -4,12 +4,22 @@ require "json"
 require "http"
 
 def get_color_palette
-  api_url = "http://colormind.io/api/"
-
-  data = HTTP.get(api_url)
-  parsed_data = JSON.parse(data)
+  var url = "http://colormind.io/api/";
+  var data = {
+    model : "default",
+    input : [[44,43,44],[90,83,82],"N","N","N"]
+  }
   
-  return parsed_data
+  var http = new XMLHttpRequest();
+  
+  http.onreadystatechange = function() {
+    if(http.readyState == 4 && http.status == 200) {
+      var palette = JSON.parse(http.responseText).result;
+    }
+  }
+  
+  http.open("POST", url, true);
+  http.send(JSON.stringify(data));
 
 end
 
