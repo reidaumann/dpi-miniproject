876cbb1d3ec71af320c8c7fd97d876ad4e232e35
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-02-01 17:47:00 +0000

    trying another api to see if i can identify the error

--- app.rb
+++ app.rb
@@ -9,17 +9,15 @@ get("/") do
 end
 
 get("/color-palette") do
-  url = URI("https://ai-color-generator.p.rapidapi.com/generate-color")
+  url = URI("https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=movies&count=10")
 
   http = Net::HTTP.new(url.host, url.port)
   http.use_ssl = true
-
+  
   request = Net::HTTP::Post.new(url)
-  request["content-type"] = 'application/json'
   request["X-RapidAPI-Key"] = '39f0b3a00dmsh6ec3586d033e418p1e6320jsn5b6729d40c32'
-  request["X-RapidAPI-Host"] = 'ai-color-generator.p.rapidapi.com'
-  request.body = "{\r\n    \"colorList\": [\r\n        \"#FBE18F\",\r\n        \"#1F271B\"\r\n    ]\r\n}"
-
+  request["X-RapidAPI-Host"] = 'andruxnet-random-famous-quotes.p.rapidapi.com'
+  
   response = http.request(request)
   puts response.read_body
   erb(:palette_gen)
